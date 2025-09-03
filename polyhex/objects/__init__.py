84c8e902717d45a154a19e15a0bef36c5cafa5a5
emilien <emilienvalat@gmail.com>
2025-09-03 08:48:20 +0200

    Forgot to update the __init__.py

--- polyhex/objects/__init__.py
+++ polyhex/objects/__init__.py
@@ -3,11 +3,10 @@ from .nodes import *
 from .edges import *
 from .hexagons import *
 from .polyhexes import *
-
+from .graphs import *
 
 __all__ = ()
 __all__ += nodes.__all__
 __all__ += edges.__all__
 __all__ += hexagons.__all__
 __all__ += polyhexes.__all__
-__all__ += decorators.__all__
